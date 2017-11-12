7073522130e07ae942e88c683b7c55611adb5022
César A. Ramírez <cesar.ramirez.sv@gmail.com>
2017-11-12 13:50:19 -0600

    Build something amazing :rocket:

---
+++ .postcssrc.js
@@ -0,0 +1,9 @@
+var tailwindcss = require('tailwindcss');
+
+module.exports = {
+  "plugins": [
+    // to edit target browsers: use "browserslist" field in package.json
+    tailwindcss('tailwind.js'),
+    require('autoprefixer'),
+  ]
+}
