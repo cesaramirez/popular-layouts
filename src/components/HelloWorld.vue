0a8c4ae784ea9cc667d82ff2e93dcea77d085404
César A. Ramírez <cesar.ramirez.sv@gmail.com>
2017-11-13 14:21:59 -0600

    Start Facebook

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -21,6 +21,7 @@
     <h2 class="my-3">Popular Layouts</h2>
     <ul class="list-reset flex justify-center">
       <li><router-link class="text-green no-underline mx-3" to="/youtube">Youtube</router-link></li>      
+      <li><router-link class="text-green no-underline mx-3" to="/facebook">Facebook</router-link></li>      
     </ul>
   </div>
 </template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
-import YoutubeComponent from '@/components/YoutubeComponent'
+import YoutubeComponent from '@/components/Layouts/YoutubeComponent'
+import FacebookComponent from '@/components/Layouts/FacebookComponent'
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/youtube',
       name: 'YoutubeComponent',
       component: YoutubeComponent
+    },
+    {
+      path: '/facebook',
+      name: 'FacebookComponent',
+      component: FacebookComponent
     }
   ]
 })
