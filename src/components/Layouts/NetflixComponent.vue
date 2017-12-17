acb66d25c1aaace4f9d87517b82728ad1410cce6
César A. Ramírez <cesar.ramirez.sv@gmail.com>
2017-12-17 14:10:49 -0600

    Add Header Netflix

--- src/components/Layouts/NetflixComponent.vue
+++ src/components/Layouts/NetflixComponent.vue
@@ -1,17 +1,18 @@
 <template>
   <div class="h-auto">
     <nav class="h-12 bg-black flex items-center w-screen">
-      <div class="container mx-auto flex">
-        <div class="flex-1 items-center w-1/2 justify-between justify-start">
-          <div class="h-8 bg-red w-12 rounded-sm"></div>
-          <div class="h-4 bg-red w-12 rounded-sm"></div>
-          <div class="h-4 bg-red w-12 rounded-sm"></div>
+      <div class="container mx-auto  flex justify-between">
+        <div class="flex items-center">
+          <router-link to="/" class="h-8 bg-red w-32 rounded-sm mr-8"></router-link>
+          <div class="h-4 bg-red w-12 rounded-sm mr-8"></div>
+          <div class="h-4 bg-white w-6 rounded-sm mr-8"></div>
+          <div class="h-4 bg-white w-6 rounded-sm"></div>
         </div>
-        <div class="flex-1 items-center w-1/2 justify-between justify-end">
-          <div class="h-4 bg-red w-12 rounded-sm"></div>
-          <div class="h-6 bg-red w-6 rounded-sm"></div>
-          <div class="h-6 bg-red w-6 rounded-sm"></div>
-          <div class="h-4 bg-red w-12 rounded-sm"></div>
+        <div class="flex items-center">
+          <div class="h-4 bg-white w-12 rounded-sm mr-4"></div>
+          <div class="h-6 bg-white w-6 rounded-sm mr-4"></div>
+          <div class="h-6 bg-orange w-6 rounded-sm mr-4"></div>
+          <div class="h-4 bg-white w-12 rounded-sm"></div>
         </div>
       </div>
     </nav>
